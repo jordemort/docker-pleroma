69051c05fb371fa1b92c498c31ad59e2e3da241b
Jordan Webb <jordan@getseam.com>
2021-02-01 23:59:20 -0600

    Move db polling after config generation

--- run-pleroma.sh
+++ run-pleroma.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
+if [ ! -e "$PLEROMA_CONFIG_PATH" ] ; then
+  generate-pleroma-config.sh
+fi
+
 while ! pg_isready -U "${POSTGRES_USER:-pleroma}" -d "postgres://${POSTGRES_HOST:-postgres}:5432/${POSTGRES_DB:-pleroma}" -t 1; do
   echo "Waiting for ${POSTGRES_HOST-postgres} to come up..." >&2
   sleep 1s
 done
-
-if [ ! -e "$PLEROMA_CONFIG_PATH" ] ; then
-  generate-pleroma-config.sh
-fi
 
 pleroma_ctl migrate
 
