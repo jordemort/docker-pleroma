ce7b088ba26da1556d238598b143ca17f30fac42
Jordan Webb <jordan@getseam.com>
2021-02-01 23:46:45 -0600

    Delete .sql files since migrate handles it

--- without-rum.sql
+++
@@ -1,4 +0,0 @@
---Extensions made by ecto.migrate that need superuser access
-CREATE EXTENSION IF NOT EXISTS citext;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
